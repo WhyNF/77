6e23aafbf14bf6dee9fd75eec1ac6cd8ffef62e6
WhyNF <wahyulupapw@gmail.com>
2025-08-12 10:19:04 +0800

    Update unband.py

--- unband.py
+++ unband.py
@@ -14,7 +14,7 @@ nomor = input("This is unBanned Script !(example: 62xxxxx): ")
 
 # List teks yang mau diacak
 BODIES = [
-    "salam whatsapp developeri kiminse whatsapp hesabını deaktiv etsin whatsapp proqramında narkotik formasında qadağan olunmuş əşyalar satdığı üçün qadağan olunmuş əşyaların satışının sayını azaltmaq və zərər görməmək üçün həmin şəxsin whatsapp hesabını deaktiv etməyi xahiş edirəm millət və dövlət nəsli, Mark Zukerberqdən kömək və whatsapp tərtibatçılarından xahiş edirəm, qadağan olunmuş əşyaların satıcısının whatsapp nömrəsi :{nomor}-dir mənə kömək etdikləri üçün Mark Zukerberqə və whatsapp tərtibatçısına təşəkkür edirəm.",
+    """salam whatsapp developeri kiminse whatsapp hesabını deaktiv etsin whatsapp proqramında narkotik formasında qadağan olunmuş əşyalar satdığı üçün qadağan olunmuş əşyaların satışının sayını azaltmaq və zərər görməmək üçün həmin şəxsin whatsapp hesabını deaktiv etməyi xahiş edirəm millət və dövlət nəsli, Mark Zukerberqdən kömək və whatsapp tərtibatçılarından xahiş edirəm, qadağan olunmuş əşyaların satıcısının whatsapp nömrəsi :{nomor}-dir mənə kömək etdikləri üçün Mark Zukerberqə və whatsapp tərtibatçısına təşəkkür edirəm.",
     "γεια σας προγραμματιστής whatsapp να απενεργοποιήσει τον λογαριασμό whatsapp κάποιου επειδή έχει πουλήσει απαγορευμένα αντικείμενα με τη μορφή φαρμάκων στην εφαρμογή whatsapp, ζητώ να απενεργοποιήσετε τον λογαριασμό whatsapp αυτού του ατόμου για να μειωθεί ο αριθμός των πωλήσεων των απαγορευμένων αντικειμένων και για να μην προκληθεί ζημιά η γενιά του έθνους και του κράτους, ζητώ βοήθεια από τον Mark Zuckerberg και τους προγραμματιστές του whatsapp,
 
 ο αριθμός whatsapp του πωλητή απαγορευμένων αντικειμένων είναι {nomor}
@@ -28,7 +28,7 @@ WhatsApp номер продавца запрещенных товаров {nomo
 the whatsapp number of the seller of prohibited items is {nomor} thanks to Mark Zuckerberg and whatsapp developer for helping me.",
     "ola desenvolvedor do whatsapp para desativar a conta do whatsapp de alguem por ter vendido itens proibidos em forma de drogas no aplicativo whatsapp, peço a desativação da conta do whatsapp dessa pessoa para diminuir o número de vendas de itens proibidos, e para não prejudicar a nação e a geração do estado, peço ajuda a Mark Zuckerberg e aos desenvolvedores do whatsapp,
 
-o número do whatsapp do vendedor de itens proibidos é {nomor} obrigado a Mark Zuckerberg e ao desenvolvedor do whatsapp por me"
+o número do whatsapp do vendedor de itens proibidos é {nomor} obrigado a Mark Zuckerberg e ao desenvolvedor do whatsapp por me"""
 ]
 
 JUMLAH_KIRIM = 100
